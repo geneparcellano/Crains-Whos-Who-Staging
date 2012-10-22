c85026d9fa2816fde712f9394ea9f563487e0460
Gene <gene.parcellano@vokalinteractive.com>
2012-10-22 15:17:17 -0500

    Cleanup JS

--- static/assets/js/test.js
+++ static/assets/js/test.js
@@ -5,30 +5,11 @@
 	$(document).ajaxComplete(function() {
 		obj = $.parseJSON(data.responseText);
 
-		// var names = [];
 		$.each(obj.whoswho, function(i, whoswho) {
-			// names.push(whoswho.first + ' ' + whoswho.middle + ' ' + whoswho.last);
-			var fullName = '<li><h2>'+ i +' '+ whoswho.first + ' ' + whoswho.middle + ' ' + whoswho.last +'</h2><dl><dt>Primary Company</dt><dd>'+ whoswho.primaryCo +'</dd><dt>Secondary Company</dt><dd>'+ whoswho.secondaryCo +'</dd><dt>Professional Associations</dt><dd>'+ whoswho.profAssociations +'</dd><dt>Civic Affiliations</dt><dd>'+ whoswho.civicAffilliations +'</dd><dt>Industry</dt><dd>'+ whoswho.industry +'</dd><dt>Undergraduate College</dt><dd>'+ whoswho.undergraduate +'</dd><dt>Graduate College</dt><dd>'+ whoswho.graduate +'</dd><dt>Home Town</dt><dd>'+ whoswho.hometown +' '+ whoswho.state +'</dd><dt>Biography</dt><dd><a href="'+ whoswho.url +'">Click here</a></dd></dl></li>';
-			$('ul').append(fullName);
+			var person = '<li><h2>'+ i +' '+ whoswho.first + ' ' + whoswho.middle + ' ' + whoswho.last +'</h2><dl><dt>Primary Company</dt><dd>'+ whoswho.primaryCo +'</dd><dt>Secondary Company</dt><dd>'+ whoswho.secondaryCo +'</dd><dt>Professional Associations</dt><dd>'+ whoswho.profAssociations +'</dd><dt>Civic Affiliations</dt><dd>'+ whoswho.civicAffilliations +'</dd><dt>Industry</dt><dd>'+ whoswho.industry +'</dd><dt>Undergraduate College</dt><dd>'+ whoswho.undergraduate +'</dd><dt>Graduate College</dt><dd>'+ whoswho.graduate +'</dd><dt>Home Town</dt><dd>'+ whoswho.hometown +' '+ whoswho.state +'</dd><dt>Biography</dt><dd><a href="'+ whoswho.url +'" target="_blank">Click here</a></dd></dl></li>';
+			$('ul').append(person);
 		});
-		// console.log(names);
 
-		// $('.typeahead').attr('data-source','[' + names + ']');
-
-		// $('button').on('click', function(e) {
-		// 	var searchValue = $('input').val().toLowerCase(),
-		// 		info;
-
-		// 	$('#result').hide().children().remove();
-
-		// 	$.each(obj.icons, function(i, icons) {
-		// 		if (icons.name.toLowerCase().indexOf(searchValue) !== -1) {
-		// 			var info = '<ul><li><img src="' + icons.location + '" alt="'+ icons.name +'" /></li><li>Name: <strong>'+ icons.name +'</strong></li><li>File Size: <strong>'+ icons.size +'</strong></li><li>Height: <strong>'+ icons.height +'</strong></li><li>Width: <strong>'+ icons.width +'</strong></li><li>Location: <strong>'+ icons.location +'</strong></li><li>Last Modified: <strong>'+ icons.modified +'</strong></li></ul>';
-		// 			$('#result').append(info).fadeIn();
-		// 		}
-		// 	});
-
-		// });
 
 	});
 
