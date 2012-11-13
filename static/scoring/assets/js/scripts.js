17f7ee1a58b57de805b74b16cd2109d174379952
Gene <gene.parcellano@vokalinteractive.com>
2012-11-12 20:15:06 -0600

    Scoring & Editing Feature

--- static/scoring/assets/js/scripts.js
+++ static/scoring/assets/js/scripts.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 var obj,
-	data = $.getJSON("assets/js/whoswho.json"),
+	data = $.getJSON("../v1/assets/js/whoswho.json"),
 	user = [],
 	userConnections = [];
 
@@ -23,7 +23,7 @@ function initiateAutoComplete() {
 		allData = [];
 
 	/*****************************************************************************
-	scrape all values and push into specified array
+	Collects data for specific arrays
 	*****************************************************************************/
 	function pushData(id, newArray) {
 		if (id[0] !== undefined && id[0].length > 1) {
@@ -110,19 +110,10 @@ function initiateAutoComplete() {
 	});
 
 	// hide autocomplete when user enters custom term
-	$('#whos-who-2012').on('keyup', 'input[type="text"]', function (e) {
-		if(e.which === 13) {
+	$('#whos-who-2012').on('keyup', 'input[type="text"]', function (event) {
+		if(event.which === 13) {
 			$(".ui-autocomplete").hide();
 		}
-	});
-}
-
-/*****************************************************************************
-Build User Profile
-*****************************************************************************/
-function runFilter() {
-	$('input[type="text"], select').blur(function() {
-		buildUserProfile();
 	});
 }
 
@@ -151,6 +142,7 @@ function buildUserProfile() {
 		userCivicAffil.push($(this).text().slice(0, -6));
 	});
 
+	//Set values
 	obj[a[0]] = userFirst;
 	obj[a[1]] = userLast;
 	obj[a[2]] = userSuffix;
@@ -169,10 +161,17 @@ function buildUserProfile() {
 
 	//Update Score
 	updateScore();
+}
 
-	// console.log('hello');
-
-	return user;
+/*****************************************************************************
+Show "Your Connections"
+*****************************************************************************/
+function runFilter() {
+	$('input[type="text"], select').blur(function() {
+		buildUserProfile();
+		loadResults(userConnections, '#all-connections');
+		animatePersonIn('#all-connections');
+	});
 }
 
 /*****************************************************************************
@@ -225,15 +224,12 @@ function updateScore() {
 		$.each(userConnections, function(i, number) {
 			var person = obj.whoswho[number];
 			if (person.pwr50 === true) {
-				// console.log('power 50');
 				totalScore += 10;
 			}
 
 			if (person.last === "Obama") {
-				// console.log('obama');
 				totalScore += 15;
 			}
-			// console.log(person);
 		});
 		var finalScore = parseInt(totalScore) + parseInt(totalMatch);
 
@@ -282,23 +278,28 @@ function updateScore() {
 		});
 	});
 
+	// Score special connections (Power 50 & Obama's)
 	specialConnections();
 }
 
 /*****************************************************************************
-Build industry list (<select>)
+Build industry list
 *****************************************************************************/
 function buildSelectOption() {
 	$.each(obj.industries, function(i, industries) {
-		var html = '<option value="'+industries+'">'+industries+'</option>';
+		var html = '<option value="' + industries + '">' + industries + '</option>';
 		$('.industry-list').append(html);
 	});
 }
 
 /*****************************************************************************
-Add Entry
+Multiple Entry Fields
 *****************************************************************************/
-function addEntry() {
+function multipleEntry() {
+
+	/*****************************************************************************
+	Apply Entry
+	*****************************************************************************/
 	function applyEntry($this) {
 		var container = $this.parents('.multi'),
 			entry = container.find('input[type="text"]'),
@@ -324,6 +325,9 @@ function addEntry() {
 		}
 	}
 
+	/*****************************************************************************
+	Apply entry and update score
+	*****************************************************************************/
 	$('#questions .multi').on('click', 'button[data-function="add"]', function() {
 		applyEntry($(this));
 		buildUserProfile();
@@ -354,13 +358,217 @@ function removeEntry() {
 }
 
 /*****************************************************************************
+Load Results
+*****************************************************************************/
+function loadResults(arrayName, resultContainer) {
+	var container = $(resultContainer),
+		emptyContainer = container.children('li').addClass('old');
+
+	$.each(arrayName, function(i, id) {
+		var	person = obj.whoswho[id],
+			htmlImage,
+			htmlPwr50,
+			htmlName,
+			htmlPrimaryCo,
+			htmlSecondaryCo,
+			htmlIndustry,
+			htmlUndergrad,
+			htmlGrad,
+			htmlHometown,
+			htmlProfAssoc,
+			htmlCivicAffil,
+			htmlBio,
+			profAssoc,
+			civicAffil,
+			formattedProfAssoc = [],
+			formattedCivicAffil = [];
+
+		function formatData(array, property, newArray) {
+			if (array[0] !== undefined && array[0].length > 1) {
+				$.each(array, function(i, property) {
+					newArray.push(' ' + property);
+				});
+			} else {
+				// do nothing
+			}
+		}
+
+		// Power 50
+		if (person.pwr50) {
+			htmlPwr50 = '<div class="pwr50">Power 50</div>';
+		} else {
+			htmlPwr50 = '';
+		}
+
+		// Primary Company
+		if (person.primaryCo) {
+			htmlPrimaryCo = '<dl><dt>Primary Company</dt><dd>'+ person.primaryCo +'</dd>';
+		} else {
+			htmlPrimaryCo = '<dl><dt>Primary Company</dt><dd>--</dd>'
+		}
+
+		// Secondary Company
+		if (person.secondaryCo) {
+			htmlSecondaryCo = '<dt>Secondary Company</dt><dd>'+ person.secondaryCo +'</dd>';
+		} else {
+			htmlSecondaryCo = '<dt>Secondary Company</dt><dd>--</dd>';
+		}
+
+		// Hometown
+		if (person.city && person.state) {
+			htmlHometown = '<dt>Home Town</dt><dd>'+ person.city +', '+ person.state +'</dd>';
+		} else if (person.city) {
+			htmlHometown = '<dt>Home Town</dt><dd>'+ person.city +'</dd>';
+		} else if (person.state) {
+			htmlHometown = '<dt>Home Town</dt><dd>'+ person.state +'</dd>';
+		} else {
+			htmlHometown = '<dt>Home Town</dt><dd>--</dd>';
+		}
+
+		// Industry
+		if (person.industry) {
+			htmlIndustry = '<dt>Industry</dt><dd>'+ person.industry +'</dd>';
+		} else {
+			htmlIndustry = '<dt>Industry</dt><dd>--</dd>';
+		}
+
+		// Undergraduate College
+		if (person.undergrad) {
+			htmlUndergrad = '<dt>Undergraduate College</dt><dd>'+ person.undergrad +'</dd>';
+		} else {
+			htmlUndergrad = '<dt>Undergraduate College</dt><dd>--</dd>';
+		}
+
+		// Graduate College
+		if (person.grad) {
+			htmlGrad = '<dt>Graduate College</dt><dd>'+ person.grad +'</dd>';
+		} else {
+			htmlGrad = '<dt>Graduate College</dt><dd>--</dd>';
+		}
+
+		// Professional Association
+		if (person.profAssoc) {
+			formatData(person.profAssoc, profAssoc, formattedProfAssoc);
+			htmlProfAssoc = '<dt>Professional Associations</dt><dd>'+ formattedProfAssoc +'</dd>';
+		} else {
+			htmlProfAssoc = '<dt>Professional Associations</dt><dd>--</dd>';
+		}
+
+		// Civic Affiliation
+		if (person.civicAffil) {
+			formatData(person.civicAffil, civicAffil, formattedCivicAffil);
+			htmlCivicAffil = '<dt>Civic Affiliations</dt><dd>'+ formattedCivicAffil +'</dd>';
+		} else {
+			htmlCivicAffil = '<dt>Civic Affiliations</dt><dd>--</dd>';
+		}
+
+		// Biography Link
+		if (person.bio) {
+			htmlBio = '<dt>Biography</dt><dd><a href="'+ person.bio +'" target="_blank">Click here</a></dd></dl>';
+		} else {
+			htmlBio = '<dt>Biography</dt><dd>--</dd></dl>';
+		}
+
+		var htmlImage = '<div class="photo"><img src="../assets/im/media/' + person.img + '" height="100" width="83" alt="" /></div>',
+			htmlName = '<h2><span>'+ person.first + ' ' + person.middle + ' </span>' + person.last +'</h2>',
+			personDetails =
+				'<li data-whoswho-id="'+ id +'">'+
+					htmlImage +
+					htmlPwr50 +
+					htmlName +
+					htmlPrimaryCo +
+					htmlSecondaryCo +
+					htmlIndustry +
+					htmlUndergrad +
+					htmlGrad +
+					htmlHometown +
+					htmlProfAssoc +
+					htmlCivicAffil +
+					htmlBio +
+				'</li>';
+
+		if (container.find('[data-whoswho-id="'+ id +'"]').length > 0) {
+			container.find('[data-whoswho-id="'+ id +'"]').removeClass('old');
+		} else {
+			container.append(personDetails);
+		}
+	});
+	animatePersonOut(resultContainer);
+}
+
+/*****************************************************************************
+Animate Grid
+*****************************************************************************/
+function animatePersonIn(resultContainer) {
+	if ($(resultContainer + ' li:hidden').length > 0) {
+		$(resultContainer + ' li:hidden').first().show('drop', {direction : 'right', easing : 'linear'}, 100, function() {
+			animatePersonIn(resultContainer);
+		});
+	}
+}
+function animatePersonOut(resultContainer) {
+	var container = $(resultContainer);
+	container.children('.old').delay(300).hide('drop', {direction : 'left', easing : 'linear'}, function() {
+		container.children('.old').remove();
+	});
+}
+
+/*****************************************************************************
+Overlay
+*****************************************************************************/
+var overlayWrap = $('.overlay'),
+	overlay = {
+	details : function() {
+		$('#whos-who').on('click', 'li', function() {
+			var $this = $(this),
+				content = $this.html(),
+				details = '<div id="overlay-whos-who-details" class="overlay-item selected">'
+						+ '<div class="controls"><strong>Details</strong><button type="button" data-function="remove">close</button></div>'
+						+ '<div class="content">'+content+'</div>'
+						+ '</div>';
+			$('.overlay-main').append(details);
+			overlay.launchItem('overlay-whos-who-details');
+		});
+	},
+	hideItem : function() {
+		overlayWrap.on('click', '[data-function="close"], .controls button[data-function="remove"]', function() {
+			var hideOverlay = overlayWrap.fadeOut('fast').parents('body').removeClass('no-scroll'),
+				dataFunc = $(this).attr('data-function');
+
+			if (dataFunc === 'remove') {
+				$('#overlay-whos-who-details').remove();
+				hideOverlay;
+			} else {
+				hideOverlay;
+			}
+		});
+	},
+	getName : function() {
+		$('body').on('click', '[data-function^="overlay"]', function(event) {
+			overlay.launchItem($(this).attr('data-function'));
+			event.preventDefault();
+		});
+	},
+	launchItem : function(itemName) {
+		if (overlayWrap.not(':visible')) {
+			overlayWrap.fadeIn('fast').parents('body').addClass('no-scroll');
+		} else {
+			// do nothing
+		}
+
+		$('#' + itemName).siblings().removeClass('selected');
+		overlayWrap.find('#' + itemName).addClass('selected');
+	}
+}
+
+/*****************************************************************************
 Initialize
 *****************************************************************************/
 $(document).ajaxComplete(function() {
 	obj = $.parseJSON(data.responseText);
 	buildSelectOption();
 	initiateAutoComplete();
-	addEntry();
+	multipleEntry();
 	removeEntry();
 });
 
@@ -369,4 +577,7 @@ $('body').on('click', '[data-function="done"]', function() {
 });
 
 runFilter();
+overlay.details();
+overlay.getName();
+overlay.hideItem();
 })();
